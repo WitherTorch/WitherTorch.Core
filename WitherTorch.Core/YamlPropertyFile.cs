906496f92ae0043679cc4f6e2c7a2a83ed7373ff
Error 404 <36844888+new1271@users.noreply.github.com>
2022-08-10 19:58:48 +0800

    改進 YAML 讀取

--- WitherTorch.Core/YamlPropertyFile.cs
+++ WitherTorch.Core/YamlPropertyFile.cs
@@ -11,8 +11,6 @@ namespace WitherTorch.Core
     public class YamlPropertyFile : JsonPropertyFile
     {
         protected static IDeserializer yamlDeserializer;
-        protected static ISerializer yamlToJsonSerializer;
-        protected static JsonSerializer jsonExpandoSerializer;
         protected static ISerializer yamlSerializer;
 
         public YamlPropertyFile(string path, bool create = true, bool ignoreLazyRequest = false) : base(path, create, ignoreLazyRequest)
@@ -27,54 +25,14 @@ namespace WitherTorch.Core
                 {
                     try
                     {
-                        if (jsonSerializer == null)
-                        {
-                            jsonSerializer = JsonSerializer.CreateDefault();
-                            jsonSerializer.Formatting = Formatting.Indented;
-                        }
                         if (yamlDeserializer == null)
                         {
                             yamlDeserializer = new DeserializerBuilder().Build();
                         }
-                        if (yamlToJsonSerializer == null)
+                        Dictionary<object, object> yamlObject = yamlDeserializer.Deserialize(reader) as Dictionary<object, object>;
+                        if (yamlObject != null)
                         {
-                            yamlToJsonSerializer = new SerializerBuilder().JsonCompatible().Build();
-                        }
-                        object yamlObject = yamlDeserializer.Deserialize(reader);
-                        string jsonString = yamlToJsonSerializer.Serialize(yamlObject);
-                        using (StringReader stringReader = new StringReader(jsonString))
-                        {
-                            using (JsonTextReader jsonReader = new JsonTextReader(stringReader))
-                            {
-                                object content = jsonSerializer.Deserialize(jsonReader);
-                                if (content is JObject obj)
-                                {
-                                    currentObject = obj;
-                                }
-                                else
-                                {
-                                    try
-                                    {
-                                        currentObject = new JObject(content);
-                                    }
-                                    catch (Exception)
-                                    {
-                                    }
-                                }
-                            }
-                            try
-                            {
-                                stringReader.Close();
-                            }
-                            catch (IOException)
-                            {
-                            }
-                            catch (NullReferenceException)
-                            {
-                            }
-                            catch (ObjectDisposedException)
-                            {
-                            }
+                            currentObject = JObject.FromObject(yamlObject);
                         }
                     }
                     catch (Exception)
@@ -109,42 +67,15 @@ namespace WitherTorch.Core
             }
             if ((isDirty && isInitialized) || force)
             {
-                if (jsonSerializer == null)
-                {
-                    jsonSerializer = JsonSerializer.CreateDefault();
-                    jsonSerializer.Formatting = Formatting.Indented;
-                }
-                if (jsonExpandoSerializer == null)
-                {
-                    jsonExpandoSerializer = JsonSerializer.CreateDefault(new JsonSerializerSettings() { Converters = new List<JsonConverter>() { new ExpandoObjectConverter() } });
-                }
                 if (yamlSerializer == null)
                 {
                     yamlSerializer = new SerializerBuilder().Build();
-                }
-                System.Dynamic.ExpandoObject deserializedObject;
-                using (StringWriter sw = new StringWriter())
-                {
-                    using (JsonTextWriter writer = new JsonTextWriter(sw))
-                    {
-                        jsonSerializer.Serialize(writer, currentObject);
-                    }
-                    sw.Flush();
-                    using (StringReader sr = new StringReader(sw.ToString()))
-                    {
-                        using (JsonTextReader reader = new JsonTextReader(sr))
-                        {
-                            deserializedObject = jsonExpandoSerializer.Deserialize<System.Dynamic.ExpandoObject>(reader);
-                        }
-                        sr.Close();
-                    }
-                    sw.Close();
                 }
                 using (StreamWriter writer = new StreamWriter(new FileStream(_path, System.IO.FileMode.Create, System.IO.FileAccess.Write)))
                 {
                     try
                     {
-                        yamlSerializer.Serialize(writer, deserializedObject);
+                        yamlSerializer.Serialize(writer, currentObject.ToObject<System.Dynamic.ExpandoObject>());
                         writer.Flush();
                         writer.Close();
                     }
