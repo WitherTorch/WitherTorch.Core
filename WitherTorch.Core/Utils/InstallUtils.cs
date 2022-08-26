680c3c697701ec3165f7057ed3340bdb34b963b1
Error 404 <36844888+new1271@users.noreply.github.com>
2022-08-26 14:14:47 +0800

    .

--- WitherTorch.Core/Utils/InstallUtils.cs
+++ WitherTorch.Core/Utils/InstallUtils.cs
@@ -7,17 +7,20 @@ namespace WitherTorch.Core.Utils
 {
     internal static class InstallUtils
     {
+        const int BUFFER_SIZE = 81920;
+
         public static async Task HttpDownload(System.Net.Http.HttpClient client, string downloadURL, string filePath)
         {
             Stream dataStream = await client.GetStreamAsync(new Uri(downloadURL));
             FileStream fileStream = new FileStream(Path.Combine(), FileMode.Create);
-            byte[] buffer = new byte[1 << 20];
-            while (true)
+            byte[] buffer = new byte[BUFFER_SIZE];
+                while (true)
             {
-                int length = dataStream.Read(buffer, 0, buffer.Length);
+                int length = dataStream.Read(buffer, 0, BUFFER_SIZE);
                 if (length > 0)
                 {
                     fileStream.Write(buffer, 0, length);
+                    fileStream.Flush();
                 }
                 else
                 {
