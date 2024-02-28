d8a6e7cd6aa379dd0f25082818488c8567481642
Error 404 <36844888+new1271@users.noreply.github.com>
2024-02-29 01:08:51 +0800

    Updated GlobalAssemblyInfo

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,8 +1,14 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyCompany("WitherTorch 製作組")]
 [assembly: AssemblyProduct("WitherTorch")]
 [assembly: AssemblyCopyright("Copyright © WitherTorch 製作組 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.8.1.14")]
+[assembly: AssemblyVersion("1.8.2.15")]
+
+//Used for NGen pre-linking
+[assembly: Dependency("System.IO.Compression", LoadHint.Always)]
+[assembly: Dependency("Newtonsoft.Json", LoadHint.Always)]
+[assembly: Dependency("YamlDotNet", LoadHint.Always)]
