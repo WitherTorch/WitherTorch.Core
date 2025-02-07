d01c154ab454408c89e7bd87d206fdfd79d4f4bd
米茶 <new1271@outlook.com>
2025-02-08 00:27:09 +0800

    將 Server 類別的內容進行完整分類，並補齊 LoadServer 的 API

---
+++ WitherTorch.Core/Server.Internal.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+using WitherTorch.Core.Property;
+using WitherTorch.Core.Software;
+
+namespace WitherTorch.Core
+{
+    partial class Server
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static Server? CreateServerInstance(ISoftwareContext software, string serverDirectory)
+        {
+            Server? server = software.CreateServerInstance(serverDirectory);
+            if (server is null)
+                return null;
+            if (!software.GetServerType().IsAssignableFrom(server.GetType()))
+            {
+                (server as IDisposable)?.Dispose();
+                return null;
+            }
+            return server;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static T? CreateServerInstanceTyped<T>(ISoftwareContext software, string serverDirectory) where T : Server
+        {
+            Server? rawServer = software.CreateServerInstance(serverDirectory);
+            if (rawServer is null)
+                return null;
+            if (rawServer is not T server)
+            {
+                (rawServer as IDisposable)?.Dispose();
+                return null;
+            }
+            return server;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static Server? CreateServerCore(ISoftwareContext software, string serverDirectory)
+        {
+            Server? server = CreateServerInstance(software, serverDirectory);
+            if (server is null)
+                return null;
+            server.ServerName = GetDefaultServerNameCore(Path.GetFullPath(serverDirectory));
+            if (!server.CreateServerCore())
+            {
+                (server as IDisposable)?.Dispose();
+                return null;
+            }
+            return server;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static T? CreateServerCoreTyped<T>(ISoftwareContext software, string serverDirectory) where T : Server
+        {
+            T? server = CreateServerInstanceTyped<T>(software, serverDirectory);
+            if (server is null)
+                return null;
+            server.ServerName = GetDefaultServerNameCore(Path.GetFullPath(serverDirectory));
+            if (!server.CreateServerCore())
+            {
+                (server as IDisposable)?.Dispose();
+                return null;
+            }
+            return server;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static Server? LoadServerCore(ISoftwareContext software, string serverDirectory, JsonPropertyFile serverInfoJson)
+        {
+            Server? server = CreateServerInstance(software, serverDirectory);
+            if (server is null)
+                return null;
+            server.ServerInfoJson = serverInfoJson;
+            server.ServerName = serverInfoJson["name"]?.GetValue<string>() ?? GetDefaultServerNameCore(Path.GetFullPath(serverDirectory));
+            if (!server.LoadServerCore(serverInfoJson))
+            {
+                (server as IDisposable)?.Dispose();
+                return null;
+            }
+            return server;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static T? LoadServerCoreTyped<T>(ISoftwareContext software, string serverDirectory, JsonPropertyFile serverInfoJson) where T : Server
+        {
+            T? server = CreateServerInstanceTyped<T>(software, serverDirectory);
+            if (server is null)
+                return null;
+            server.ServerInfoJson = serverInfoJson;
+            server.ServerName = serverInfoJson["name"]?.GetValue<string>() ?? GetDefaultServerNameCore(Path.GetFullPath(serverDirectory));
+            if (!server.LoadServerCore(serverInfoJson))
+            {
+                (server as IDisposable)?.Dispose();
+                return null;
+            }
+            return server;
+        }
+
+        private static string GetDefaultServerNameCore(string serverDirectory)
+        {
+#if NET5_0_OR_GREATER
+            ReadOnlySpan<char> span = serverDirectory.AsSpan().TrimEnd(Path.DirectorySeparatorChar);
+            if (span.Length > 3)
+                return Path.GetFileName(span).ToString();
+#else
+            serverDirectory = serverDirectory.TrimEnd(Path.DirectorySeparatorChar);
+            if (serverDirectory.Length > 3)
+                return Path.GetFileName(serverDirectory);
+#endif
+            return serverDirectory;
+        }
+    }
+}
---
+++ WitherTorch.Core/Server.Static.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Nodes;
+
+using WitherTorch.Core.Property;
+using WitherTorch.Core.Software;
+
+namespace WitherTorch.Core
+{
+    partial class Server
+    {
+        /// <summary>
+        /// 以指定的伺服器物件類型與伺服器資料夾路徑來建立新的伺服器
+        /// </summary>
+        /// <typeparam name="T">伺服器類型，需使用 <see cref="SoftwareRegister.TryRegisterServerSoftware(ISoftwareContext)"/> 註冊</typeparam>
+        /// <param name="serverDirectory">伺服器資料夾的路徑</param>
+        /// <returns>建立好的伺服器，或是 <see langword="null"/> (如果建立伺服器時發生問題的話)</returns>
+        public static T? CreateServer<T>(string serverDirectory) where T : Server
+        {
+            ISoftwareContext? software = SoftwareRegister.GetSoftwareContext(typeof(T), throwExceptionIfNotRegistered: true);
+            if (software is null)
+                return null;
+            return CreateServerCoreTyped<T>(software, serverDirectory);
+        }
+
+        /// <summary>
+        /// 以指定的伺服器物件類型與伺服器資料夾路徑來建立新的伺服器
+        /// </summary>
+        /// <param name="serverType">伺服器類型，需繼承 <see cref="Server"/> 並使用 <see cref="SoftwareRegister.TryRegisterServerSoftware(ISoftwareContext)"/> 註冊</param>
+        /// <param name="serverDirectory">伺服器資料夾的路徑</param>
+        /// <returns>建立好的伺服器，或是 <see langword="null"/> (如果建立伺服器時發生問題的話)</returns>
+        public static Server? CreateServer(Type serverType, string serverDirectory)
+        {
+            ISoftwareContext? software = SoftwareRegister.GetSoftwareContext(serverType, throwExceptionIfNotRegistered: true);
+            if (software is null)
+                return null;
+            return CreateServerCore(software, serverDirectory);
+        }
+
+        /// <summary>
+        /// 建立伺服器
+        /// </summary>
+        /// <param name="softwareId">軟體的ID</param>
+        /// <param name="serverDirectory">伺服器路徑</param>
+        /// <returns>建立好的伺服器</returns>
+        /// <exception cref="ServerSoftwareIsNotRegisteredException"/>
+        public static Server? CreateServer(string softwareId, string serverDirectory)
+        {
+            ISoftwareContext? software = SoftwareRegister.GetSoftwareContext(softwareId, throwExceptionIfNotRegistered: true);
+            if (software is null)
+                return null;
+            return CreateServerCore(software, serverDirectory);
+        }
+
+
+        /// <summary>        
+        /// 載入位於指定路徑內的伺服器
+        /// </summary>
+        /// <param name="serverDirectory">伺服器資料夾路徑</param>
+        /// <exception cref="ServerSoftwareIsNotRegisteredException"/>
+        /// <returns>指定的伺服器，若伺服器不存在則為 <see langword="null"/></returns>
+        public static Server? LoadServer(string serverDirectory)
+        {
+            string path = Path.Combine(serverDirectory, "./server_info.json");
+            if (!File.Exists(path))
+                return null;
+            JsonPropertyFile serverInformation = new JsonPropertyFile(path, useFileWatcher: false);
+            ISoftwareContext? software = SoftwareRegister.GetSoftwareContext(serverInformation["software"]?.GetValue<string>(), throwExceptionIfNotRegistered: true);
+            if (software is null)
+                return null;
+            return LoadServerCore(software, serverDirectory, serverInformation);
+        }
+
+        /// <summary>
+        /// 載入位於指定路徑內的伺服器，並將其指定為 <paramref name="softwareId"/> 所對應的伺服器軟體
+        /// </summary>
+        /// <param name="softwareId">伺服器軟體 ID</param>
+        /// <param name="serverDirectory">伺服器資料夾路徑</param>
+        /// <returns>指定的伺服器，若伺服器不存在則為 <see langword="null"/></returns>
+        public static Server? LoadServer(string softwareId, string serverDirectory)
+        {
+            ISoftwareContext? software = SoftwareRegister.GetSoftwareContext(softwareId, throwExceptionIfNotRegistered: true);
+            if (software is null)
+                return null;
+            string path = Path.Combine(serverDirectory, "./server_info.json");
+            if (!File.Exists(path))
+                return null;
+            return LoadServerCore(software, serverDirectory, new JsonPropertyFile(path, useFileWatcher: false));
+        }
+
+        /// <summary>
+        /// 載入位於指定路徑內的伺服器，並將其指定為 <paramref name="serverType"/> 所對應的伺服器軟體
+        /// </summary>
+        /// <param name="serverType">伺服器類型，需繼承 <see cref="Server"/> 並使用 <see cref="SoftwareRegister.TryRegisterServerSoftware(ISoftwareContext)"/> 註冊</param>
+        /// <param name="serverDirectory">伺服器資料夾路徑</param>
+        /// <returns>指定的伺服器，若伺服器不存在則為 <see langword="null"/></returns>
+        public static Server? LoadServer(Type serverType, string serverDirectory)
+        {
+            ISoftwareContext? software = SoftwareRegister.GetSoftwareContext(serverType, throwExceptionIfNotRegistered: true);
+            if (software is null)
+                return null;
+            string path = Path.Combine(serverDirectory, "./server_info.json");
+            if (!File.Exists(path))
+                return null;
+            return LoadServerCore(software, serverDirectory, new JsonPropertyFile(path, useFileWatcher: false));
+        }
+
+        /// <summary>
+        /// 載入位於指定路徑內的伺服器，並將其指定為 <typeparamref name="T"/> 所對應的伺服器軟體
+        /// </summary>
+        /// <typeparam name="T">伺服器類型，需使用 <see cref="SoftwareRegister.TryRegisterServerSoftware(ISoftwareContext)"/> 註冊</typeparam>
+        /// <param name="serverDirectory">伺服器資料夾路徑</param>
+        /// <returns>指定的伺服器，若伺服器不存在則為 <see langword="null"/></returns>
+        public static T? LoadServer<T>(string serverDirectory) where T : Server
+        {
+            ISoftwareContext? software = SoftwareRegister.GetSoftwareContext(typeof(T), throwExceptionIfNotRegistered: true);
+            if (software is null)
+                return null;
+            string path = Path.Combine(serverDirectory, "./server_info.json");
+            if (!File.Exists(path))
+                return null;
+            return LoadServerCoreTyped<T>(software, serverDirectory, new JsonPropertyFile(path, useFileWatcher: false));
+        }
+
+        /// <summary>
+        /// 取得預設的伺服器名稱 (通常是伺服器資料夾的名字)
+        /// </summary>
+        /// <returns>預設的伺服器名稱</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string GetDefaultServerName(Server server) => GetDefaultServerNameCore(server.ServerDirectory);
+    }
+}
