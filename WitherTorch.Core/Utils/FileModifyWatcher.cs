d105baef0cd9176cee27eb9cdf78d3fa6936caa7
米茶 <new1271@outlook.com>
2025-03-06 05:26:28 +0800

    小修正

--- WitherTorch.Core/Utils/FileModifyWatcher.cs
+++ WitherTorch.Core/Utils/FileModifyWatcher.cs
@@ -145,7 +145,13 @@ namespace WitherTorch.Core.Utils
                     return;
                 if (disposing)
                     _watchers.Clear();
-                _trigger.Set();
+                try
+                {
+                    _trigger.Set();
+                }
+                catch (Exception)
+                {
+                }
             }
 
             ~WatchingThreadLoop()
