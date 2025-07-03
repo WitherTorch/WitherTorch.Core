ebf19cc1853004eafaa8ea7103e9411c766d1514
米茶 <new1271@outlook.com>
2025-07-03 22:53:39 +0800

    更新版本號

--- WitherTorch.Core/AssemblyInfo.cs
+++ WitherTorch.Core/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("Copyright © WitherTorch 製作組 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.8.3.0")]
-[assembly: AssemblyFileVersion("1.8.3.19")]
+[assembly: AssemblyVersion("1.8.4.0")]
+[assembly: AssemblyFileVersion("1.8.4.20")]
 
 //Used for NGen pre-linking
 [assembly: Dependency("System.Text.Json", LoadHint.Always)]
