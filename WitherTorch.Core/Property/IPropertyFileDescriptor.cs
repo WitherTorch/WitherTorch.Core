cc5159c31d944c050bc2ec43d0254feea0847d3d
米茶 <new1271@outlook.com>
2026-03-25 02:01:16 +0800

    小修正

--- WitherTorch.Core/Property/IPropertyFileDescriptor.cs
+++ WitherTorch.Core/Property/IPropertyFileDescriptor.cs
@@ -1,4 +1,4 @@
-﻿namespace WitherTorch.Core.Property
+namespace WitherTorch.Core.Property
 {
     /// <summary>
     /// 表示一個設定檔案的節點
@@ -53,7 +53,7 @@
         /// 取得特定的設定檔節點
         /// </summary>
         /// <param name="path">以 . 分隔的節點路徑</param>
-        PropertyFileNode GetNode(string[] path);
+        PropertyFileNode? GetNode(string[] path);
         /// <summary>
         /// 設定特定的設定檔節點
         /// </summary>
